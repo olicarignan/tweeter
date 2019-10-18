b94235f18f8058de2418cfb7ed0e902a488d7204
olicarignan <olive.carignan@gmail.com>
2019-10-18 00:08:30 +0000

    fix text size

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -102,8 +102,8 @@ function formatAMPM() {
     minutes = d.getMinutes().toString().length === 1 ? '0' + d.getMinutes() : d.getMinutes(),
     hours = d.getHours().toString().length === 1 ? '0' + d.getHours() : d.getHours(),
     ampm = d.getHours() >= 12 ? 'pm' : 'am',
-    months = ['January','February','March','April','May','June','July','August','September','October','November','December'],
-    days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec'],
+    days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
   let postTime = days[d.getDay()] + ' ' + months[d.getMonth()] + ' ' + d.getDate() + ' ' + d.getFullYear() + ' ' + hours + ':' + minutes + ampm;
   return postTime;
  };
