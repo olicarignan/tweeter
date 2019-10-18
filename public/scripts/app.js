5122217f505d53151abe77660025a475ed922ab8
olicarignan <olive.carignan@gmail.com>
2019-10-18 00:50:23 +0000

    clean up code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -94,18 +94,5 @@ const loadTweets = () => {
   })
 }
 loadTweets();
-
-// const moment = require("moment");
-
-// function formatAMPM() {
-//   let d = new Date(),
-//     minutes = d.getMinutes().toString().length === 1 ? '0' + d.getMinutes() : d.getMinutes(),
-//     hours = d.getHours().toString().length === 1 ? '0' + d.getHours() : d.getHours(),
-//     ampm = d.getHours() >= 12 ? 'pm' : 'am',
-//     months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec'],
-//     days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-//   let postTime = days[d.getDay()] + ' ' + months[d.getMonth()] + ' ' + d.getDate() + ' ' + d.getFullYear() + ' ' + hours + ':' + minutes + ampm;
-//   return postTime;
-//  };
 });
 
